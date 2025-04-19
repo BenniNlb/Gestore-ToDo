e69897e03f4830d7d4c4f6411647b81dbf5ae12d
BenniNlb <bennydox.n@icloud.com>
2025-04-19 18:06:49 +0200

    Definizione Bacheca
    È stata definita la struttura base per la gestione delle bacheche, creando la classe "Bacheca" e l'enum "TitoloBacheca" per rappresentare i principali elementi di dominio. Questa implementazione fornisce una base iniziale per la gestione e la personalizzazione delle bacheche nel sistema.

--- src/TitoloBacheca.java
+++ src/TitoloBacheca.java
@@ -1,2 +1,10 @@
-public class TitoloBacheca {
+// Enumerazione che rappresenta i soli tre titoli disponibili per una Bacheca.
+// Questo vincola i valori per evitare errori di inserimento e garantire coerenza con i requisiti.
+
+public enum TitoloBacheca {
+    UNIVERSITA,
+    LAVORO,
+    TEMPO_LIBERO
 }
+
+
